abeb507abc7d92bca7ba748fc0453abf86f8a1c3
seldon72 <ulloa.paco@gmail.com>
2017-12-25 16:10:00 -0600

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -83,6 +83,7 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
+// POST /users
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
@@ -98,6 +99,18 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
